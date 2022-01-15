c63fffe13cb68ea947cbeaae50696adacf69491d
Nikhil Nair <niknair3189@gmail.com>
2022-01-16 00:28:06 +0530

    Add firebase

--- src/helpers/firebase.js
+++ src/helpers/firebase.js
@@ -1,23 +1,19 @@
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/firestore'
 import 'firebase/compat/analytics'
-import 'firebase/compat/auth'
 
 var firebaseConfig = {
-    apiKey: "AIzaSyDLDpBQenecyN8g3i6SYMkdt2dOlOhWufE",
-    authDomain: "ideahub31.firebaseapp.com",
-    databaseURL: "https://ideahub31-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "ideahub31",
-    storageBucket: "ideahub31.appspot.com",
-    messagingSenderId: "1079537595844",
-    appId: "1:1079537595844:web:f557cfc9dd30ddb12ea091",
-    measurementId: "G-MNCWP64FFH"
+    apiKey: "AIzaSyC4rIPge2S2ZiNEgv43GOyjTa3Rt1J7iAQ",
+    authDomain: "crypto-musk.firebaseapp.com",
+    projectId: "crypto-musk",
+    storageBucket: "crypto-musk.appspot.com",
+    messagingSenderId: "730394249825",
+    appId: "1:730394249825:web:43a32c420c5ec0980af820",
+    measurementId: "G-YF1JS902JL"
 };
 firebase.initializeApp(firebaseConfig);
 firebase.analytics();
 
 export const dbref = firebase.firestore();
-export const auth = firebase.auth();
 
-export const googleProvider = new firebase.auth.GoogleAuthProvider()
 export const twitterProvider = new firebase.auth.TwitterAuthProvider();
